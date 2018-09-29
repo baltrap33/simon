3ba5ffb38e29b8c23a7f92ead2d6c0047d4f4a14
cordovado <jb.cordovado@gmail.com>
2018-09-29 22:25:57 +0200

    add Key class

---
+++ js/Key.js
@@ -0,0 +1,34 @@
+class Key {
+    // constructeur de la classe
+    constructor() {
+
+    }
+    // permet d'obtenir l'id de l'élement HTML concerné
+    get id() {
+
+    }
+    // la touche doit jouer le son et s'éclairer
+    // l'élément html <audio> peut jouer un son
+    // var a = document.getELementById('#idBaliseAudio');
+    // a.play();
+    play() {
+
+    }
+    // quand le son est fini la touche s'éteinds
+    stoped() {
+
+    }
+    // on ajoute un écouteur du click
+    addClickListener() {
+
+    }
+    // on ajoute un écouteur à l'élément <audio>
+    // pour savoir quand il a fini de jouer afin d'éteindre la lumière
+    addEndedListener() {
+
+    }
+    // on ajoute des écouteurs
+    addListeners() {
+
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var simonKeys = [];  
+    function createKeys(){
+        
+    }
+    // Utiliser la classe Key pour créer les boutons du simon
+    createKeys();
+});
