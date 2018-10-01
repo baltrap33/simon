312b44193b9c15df2ccc40f9c240254ffdb1b1c8
David <david.guyomard@live.fr>
2018-10-01 14:42:49 +0200

    cablage des boutons

--- js/Key.js
+++ js/Key.js
@@ -1,7 +1,8 @@
 class Key {
     // constructeur de la classe
-    constructor() {
-
+    constructor(buttonJquery) {
+        this.buttonJquery = buttonJquery;
+        this.addListeners();
     }
     // permet d'obtenir l'id de l'élement HTML concerné
     get id() {
@@ -12,23 +13,36 @@ class Key {
     // var a = document.getELementById('#idBaliseAudio');
     // a.play();
     play() {
-
+        var htmlEl = this.buttonJquery;
+        var son = htmlEl.children();
+        var audio = son[0];
+        audio.play();
+        htmlEl.addClass("lighted");
     }
     // quand le son est fini la touche s'éteinds
     stoped() {
-
+        this.buttonJquery.removeClass("lighted");
     }
     // on ajoute un écouteur du click
     addClickListener() {
-
+        var instance = this;
+        this.buttonJquery.click(function () {
+            instance.play();
+        });
     }
     // on ajoute un écouteur à l'élément <audio>
     // pour savoir quand il a fini de jouer afin d'éteindre la lumière
     addEndedListener() {
-
+        var htmlEl = this.buttonJquery;
+        var instance = this;
+        var son = htmlEl.children();
+        son.on('ended', function () {
+            instance.stoped();
+        });
     }
     // on ajoute des écouteurs
     addListeners() {
-
+        this.addClickListener();
+        this.addEndedListener();
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     var simonKeys = [];  
     function createKeys(){
-        
+       $(".button").each(function(){
+            var buttonJquery = $(this);
+            var key = new Key(buttonJquery);    
+            simonKeys.push(key);       
+       })
     }
     // Utiliser la classe Key pour créer les boutons du simon
     createKeys();
