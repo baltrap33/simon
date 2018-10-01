188c6e5ae0619214a48508662b1eb03ae22f328c
luc <catefil33@gmail.com>
2018-10-01 14:40:44 +0200

    câblage btn

--- js/Key.js
+++ js/Key.js
@@ -1,34 +1,52 @@
 class Key {
     // constructeur de la classe
-    constructor() {
-
+    constructor(boutonJQ) {
+        this.boutonJQ = boutonJQ;
+        this.addListeners();
     }
     // permet d'obtenir l'id de l'élement HTML concerné
     get id() {
-
+        var el = this.boutonJQ[0];
+        return el.id;
     }
     // la touche doit jouer le son et s'éclairer
     // l'élément html <audio> peut jouer un son
     // var a = document.getELementById('#idBaliseAudio');
     // a.play();
     play() {
-
+        var htmlElJQ = this.boutonJQ;
+        var a = htmlElJQ.children().first();
+        var audio = a[0];
+        audio.play();
     }
     // quand le son est fini la touche s'éteinds
     stoped() {
-
+        var htmlElJQ = this.boutonJQ;
+        htmlElJQ.removeClass('lighted');
     }
     // on ajoute un écouteur du click
     addClickListener() {
-
+        var instance = this;
+        var htmlElJQ = this.boutonJQ
+        htmlElJQ.click(function () {
+            instance.play();
+            htmlElJQ.addClass('lighted');
+        });
     }
     // on ajoute un écouteur à l'élément <audio>
     // pour savoir quand il a fini de jouer afin d'éteindre la lumière
     addEndedListener() {
-
+        var instance = this;
+        var htmlElJQ = this.boutonJQ;
+        var a = htmlElJQ.children().first();
+        var audio = a[0];
+        audio.onended = function() {
+            instance.stoped();
+        }
     }
     // on ajoute des écouteurs
     addListeners() {
-
+        this.addClickListener();
+        this.addEndedListener();
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     var simonKeys = [];  
     function createKeys(){
-        
+        $('.button').each(function(){
+            var boutonJQ = $(this);
+            var btn = new Key(boutonJQ);
+            simonKeys.push(btn);
+        });
     }
     // Utiliser la classe Key pour créer les boutons du simon
     createKeys();
+    console.log(simonKeys);
+    d = simonKeys[0];
 });
