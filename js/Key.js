9a6ef2d6e91b5c3e1336bd256a07b21833162d34
Lacquemant <sacha.lacquemant29@gmail.com>
2018-10-01 16:43:02 +0200

    Add jquery

--- js/Key.js
+++ js/Key.js
@@ -1,10 +1,19 @@
 class Key {
     // constructeur de la classe
-    constructor() {
-
+    constructor(simonKey) {
+        this.simonKey = simonKey;
+        this.mute = false;
+        this.addListeners();
     }
     // permet d'obtenir l'id de l'élement HTML concerné
     get id() {
+        return this.simonKey[0].id;
+
+    }
+    get muted(){
+    
+    }
+    set muted(){
 
     }
     // la touche doit jouer le son et s'éclairer
@@ -12,23 +21,37 @@ class Key {
     // var a = document.getELementById('#idBaliseAudio');
     // a.play();
     play() {
-
+        this.simonKey.addClass("lighted");
+        var sourceEl = $(".source", this.simonKey)[0];
+        sourceEl.play();
     }
+
     // quand le son est fini la touche s'éteinds
-    stoped() {
+    stoped(sourceEl) {
+
+        this.simonKey.removeClass("lighted");
+
 
     }
     // on ajoute un écouteur du click
     addClickListener() {
-
+        var instance = this;
+        this.simonKey.click(function () {
+            instance.play();
+        });
     }
     // on ajoute un écouteur à l'élément <audio>
     // pour savoir quand il a fini de jouer afin d'éteindre la lumière
     addEndedListener() {
-
+        var sourceEl = $(".source", this.simonKey);
+        var instance = this;
+        sourceEl.on('ended', function () {
+            instance.stoped();
+        });
     }
     // on ajoute des écouteurs
     addListeners() {
-
+        this.addClickListener();
+        this.addEndedListener();
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-    var simonKeys = [];  
-    function createKeys(){
-        
+$(document).ready(function () {
+    var simonKeys = [];
+    function createKeys() {
+        $(".button").each(function () {
+            var simonKey = new Key($(this));
+            simonKeys.push(simonKey);
+        });
+        $("#startBtn").click(function () {
+            var i = 0;
+            simonKeys;
+            for (i; simonKeys.length > i; i++) {
+                console.log(simonKeys[i].id);
+                simonKeys[i].play();
+                if (i == 0 || i == 1){
+
+                }
+            }
+            
+        });
     }
     // Utiliser la classe Key pour créer les boutons du simon
     createKeys();
