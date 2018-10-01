a990709a0a816237228fda5845f9caee16728aaa
David <david.guyomard@live.fr>
2018-10-01 16:37:51 +0200

    mise en fonction touches + muted

--- js/Key.js
+++ js/Key.js
@@ -2,11 +2,16 @@ class Key {
     // constructeur de la classe
     constructor(buttonJquery) {
         this.buttonJquery = buttonJquery;
+        this.mute = false;
         this.addListeners();
     }
     // permet d'obtenir l'id de l'élement HTML concerné
     get id() {
+        return this.buttonJquery[0].id;
+    };
 
+    get muted(){
+        return this.mute;
     }
     // la touche doit jouer le son et s'éclairer
     // l'élément html <audio> peut jouer un son
@@ -16,7 +21,10 @@ class Key {
         var htmlEl = this.buttonJquery;
         var son = htmlEl.children();
         var audio = son[0];
-        audio.play();
+        if (!this.mute){
+            audio.play();
+            console.log('play');
+        }      
         htmlEl.addClass("lighted");
     }
     // quand le son est fini la touche s'éteinds
--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
     }
     // Utiliser la classe Key pour créer les boutons du simon
     createKeys();
+
+    $("#startBtn").click(function(){
+        var i;
+        for (i=0; i<simonKeys.length; i++) {
+            if ($(".button").attr('disabled')){
+                $(this).mute();
+            }
+            console.log(simonKeys[i].muted);
+            simonKeys[i].play();
+        };
+    });
 });
