d6f1211e256908e3c6ad191ad05230cc06bf76db
cordovado <jb.cordovado@gmail.com>
2018-10-02 08:30:34 +0200

    working Keys and muted property

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     var simonKeys = [];  
     function createKeys(){
-        
+        $(".button").each(function(){
+            var elementJquery = $(this);
+            var key = new Key(elementJquery);
+            simonKeys.push(key);
+        });
     }
     // Utiliser la classe Key pour créer les boutons du simon
     createKeys();
