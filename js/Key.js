36e4440d752080622fc4ad85e04e0ac6cab77a9a
cordovado <jb.cordovado@gmail.com>
2018-10-02 21:02:52 +0200

    newVersionKeys and css

--- js/Key.js
+++ js/Key.js
@@ -1,52 +1,49 @@
 class Key {
-    // constructeur de la classe
+
     constructor(jqElement) {
         this.jqueryElement = jqElement;
+        this.url = jqElement.attr('audio-src');
         this.isMuted = this.analyseKey();
-        this.addListeners();
+        this.addClickListener();
     }
-    // permet d'obtenir l'id de l'élement HTML concerné
-    get id() {
-        var instance = this,
-            jqEl = instance.jqueryElement,
-            el = jqEl[0];
 
-        return el.id;
+    get id() {
+
+        return this.jqueryElement.attr('id');
     }
-    // permet d'obtenir l'état 'muet' d'une touche
+
     get muted() {
+
         return this.isMuted;
     }
-    // permet de rendre 'muet' ou non une touche
     set muted(mute) {
         this.isMuted = mute;
     }
 
-    // la touche doit jouer le son et s'éclairer
-    // si elle n'est pas dans l'état 'muet'
-    // l'élément html <audio> peut jouer un son
-    // var a = document.getELementById('#idBaliseAudio');
-    // a.play();
     play() {
-        var instance = this,
-            jqEl = instance.jqueryElement,
-            audioJqueryEl = jqEl.find('audio'),
-            audioEl = audioJqueryEl[0];
+        if (this.isMuted) {
 
-        if (instance.isMuted) {
             return;
         }
-        jqEl.addClass('lighted');
-        audioEl.play();
-    }
-    // quand le son de la touche est fini la touche s'éteinds
-    stoped() {
-        var instance = this,
-            jqEl = instance.jqueryElement;
 
-        jqEl.removeClass('lighted');
+        this.jqueryElement.removeClass('lighted').addClass('lighted');
+        this.createSound();
     }
-    // on ajoute un écouteur du click
+    createSound() {
+        var instance = this,
+            audio = new Audio(this.url);
+
+        audio.play();
+        audio.onended = function () {
+            instance.stoped();
+        }
+    }
+    stoped() {
+        var instance = this;
+        this.jqueryElement.fadeOut(200, function () {
+            instance.jqueryElement.removeClass('lighted').show();
+        });
+    }
     addClickListener() {
         var instance = this,
             jqEl = instance.jqueryElement;
@@ -55,28 +52,11 @@ class Key {
             instance.play();
         });
     }
-    // on ajoute un écouteur à l'élément <audio>
-    // pour savoir quand il a fini de jouer afin d'éteindre la lumière
-    addEndedListener() {
-        var instance = this,
-            jqEl = instance.jqueryElement,
-            audioJqueryEl = jqEl.find('audio');
-
-        audioJqueryEl.on('ended', function () {
-            instance.stoped();
-        });
-    }
-    // on analyse l'element html pour savoir si la touche est 'muette'
     analyseKey() {
         var instance = this,
             jqEl = instance.jqueryElement;
         var muted = jqEl.attr("muted");
 
         return muted && muted == "true";
-    }
-    // on ajoute des écouteurs
-    addListeners() {
-        this.addClickListener();
-        this.addEndedListener();
     }
 }
